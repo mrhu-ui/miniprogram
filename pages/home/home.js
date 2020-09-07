08fa96e009481caaf14d34ff7b0c7968884550f4
“ <1035641045@qq.com>
2020-09-07 14:04:36 +0800

    注册小程序和页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,59 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    name: 'mrhu',
+    students: [
+      {id: 1, name: 'zhangshan', age: 11},
+      {id: 2, name: 'lisi', age: 21},
+      {id: 3, name: 'wanwu', age: 31},
+      {id: 4, name: 'zhaoliu', age: 14}
+    ],
+    counter: 0,
+    list: []
+  },
+  increment() {
+    // 1.错误做法，界面是不会发生刷新的
+    // this.data.counter++
+    // 2.this.setDate()
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  decrement() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
+  },
+  onLoad() {
+    wx.request({
+      url: 'http://152.136.185.210:8000/api/z8/recommend',
+      success: (res) => {
+        console.log(res)
+        const data = res.data.data.list
+        this.setData({
+          list: data
+        })
+      }
+    })
+  },
+  onShow() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onReady() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  onHide() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  onUnload() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 监听页面的滚动
+  onPageScroll(obj) {
+    // console.log(obj)
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 监听页面滚动到底部
+  onReachBottom() {
+    console.log('页面滚动到底部了')
   }
 })
